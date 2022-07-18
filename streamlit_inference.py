b2b4550803439540d9eb38b374b81e0bd9b59032
Soyoung <56706291+Soyoung24@users.noreply.github.com>
2022-07-18 14:31:10 +0900

    Update streamlit_inference.py

--- streamlit_inference.py
+++ streamlit_inference.py
@@ -79,15 +79,152 @@ def inference(image_filename):
         outputs = model(inputs)
         preds = [1 if x > 0.5 else 0 for x in
                  outputs.squeeze().tolist()]  # the class with the highest energy is what we choose as prediction
+        
     return onehot2abc(np.array(preds))
 
 
+# GradCAM
+from gradcam.utils import visualize_cam
+from gradcam import GradCAM, GradCAMpp
+from torchvision.utils import make_grid, save_image
 
 
+import torch.nn.functional as F
+
+class Multi_GradCAM:
+    """Calculate GradCAM salinecy map.
+    Args:
+        input: input image with shape of (1, 3, H, W)
+        class_idx (int): class index for calculating GradCAM.
+                If not specified, the class index that makes the highest model prediction score will be used.
+    Return:
+        mask: saliency map of the same spatial dimension with input
+        logit: model output
+    A simple example:
+        # initialize a model, model_dict and gradcam
+        resnet = torchvision.models.resnet101(pretrained=True)
+        resnet.eval()
+        gradcam = GradCAM.from_config(model_type='resnet', arch=resnet, layer_name='layer4')
+        # get an image and normalize with mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+        img = load_img()
+        normed_img = normalizer(img)
+        # get a GradCAM saliency map on the class index 10.
+        mask, logit = gradcam(normed_img, class_idx=10)
+        # make heatmap from mask and synthesize saliency map using heatmap and img
+        heatmap, cam_result = visualize_cam(mask, img)
+    """
+
+    def __init__(self, arch: torch.nn.Module, target_layer: torch.nn.Module):
+        self.model_arch = arch
+
+        self.gradients = dict()
+        self.activations = dict()
+
+        def backward_hook(module, grad_input, grad_output):
+            self.gradients['value'] = grad_output[0]
+
+        def forward_hook(module, input, output):
+            self.activations['value'] = output
+
+        target_layer.register_forward_hook(forward_hook)
+        target_layer.register_backward_hook(backward_hook)
+
+    @classmethod
+    def from_config(cls, arch: torch.nn.Module, model_type: str, layer_name: str):
+        target_layer = layer_finders[model_type](arch, layer_name)
+        return cls(arch, target_layer)
+
+    def saliency_map_size(self, *input_size):
+        device = next(self.model_arch.parameters()).device
+        self.model_arch(torch.zeros(1, 3, *input_size, device=device))
+        return self.activations['value'].shape[2:]
+
+    def forward(self, input, class_idx=None, retain_graph=False):
+        b, c, h, w = input.size()
+        scores = []
+        logit = self.model_arch(input)
+        if class_idx is None:
+            score = logit[:, logit.max(1)[-1]].squeeze()
+        
+#             score = logit[:, class_idx].squeeze()
+        elif type(class_idx)== list:
+            for idx in class_idx:
+                s = logit[:, idx].squeeze()
+                scores.append(s)
+        for i, score in enumerate(scores):
+            self.model_arch.zero_grad()
+            score.backward(retain_graph=retain_graph)
+            gradients = self.gradients['value']
+            activations = self.activations['value']
+            b, k, u, v = gradients.size()
+
+            alpha = gradients.view(b, k, -1).mean(2)
+            # alpha = F.relu(gradients.view(b, k, -1)).mean(2)
+            weights = alpha.view(b, k, 1, 1)
 
 
+            saliency_map = (weights*activations).sum(1, keepdim=True)
+
+            saliency_map = F.relu(saliency_map)
+
+        
+            saliency_map = F.upsample(saliency_map, size=(h, w), mode='bilinear', align_corners=False)
+            saliency_map_min, saliency_map_max = saliency_map.min(), saliency_map.max()
+            if i == 0:
+                saliency_map2 = (saliency_map - saliency_map_min).div(saliency_map_max - saliency_map_min).data
+            else:
+                saliency_map2 += (saliency_map - saliency_map_min).div(saliency_map_max - saliency_map_min).data
+
+                    
+        return saliency_map2, logit
+
+    def __call__(self, input, class_idx=None, retain_graph=True):
+        return self.forward(input, class_idx, retain_graph)
+
+def inference(image_filename):
+    image = image_loader(image_filename)
+    #imshow(image.cpu().squeeze())
+    model.eval()
+    # since we're not training, we don't need to calculate the gradients for our outputs
+    with torch.no_grad():
+        inputs = image #.to(device)
+        outputs = model(inputs)
+        preds = [1 if x > 0.5 else 0 for x in
+                 outputs.squeeze().tolist()]  # the class with the highest energy is what we choose as prediction
+    masks = []
+    heatmaps = []
+    results = []    
+    images = []
+
+    
+    pred_label = [i-65 for i in list(map(ord, onehot2abc(np.array(preds))))] 
+    num_label = len(pred_label)
+    for i in pred_label:
+        mask, _ = gradcam(inputs, class_idx = i)
+        heatmap, result = visualize_cam(mask, image.cpu().squeeze())
+        masks.append(mask)
+        heatmaps.append(heatmap)
+        results.append(result)
+    mask, _ = multi_gradcam(inputs, class_idx = pred_label)
+    heatmap, result = visualize_cam(mask, image.cpu().squeeze())
+    masks.append(mask)
+    heatmaps.append(heatmap)
+    results.append(result)
+    
+    grid_image = make_grid(images, nrow=num_label+2)
+
+    images.extend([image.cpu().squeeze()] + results), grid_image
+
+    return onehot2abc(np.array(preds))
+
+    
+    
 st.title('블록 패턴 추출')
 model = load_model()
+
+target_layer = model._blocks[25]
+gradcam = GradCAM(model, target_layer)
+multi_gradcam = Multi_GradCAM(model, target_layer)
 
 st.markdown('📸 카메라로 직접 블록구조를 촬영하실 수 있습니다.')
 
@@ -105,9 +242,12 @@ if uploaded_file is not None:
     # st.write(os.listdir())
 
 
-    answer = inference(uploaded_file)
+    answer, grid_image = inference(uploaded_file)
 
     st.write(f"패턴: {answer}")
+
+    if st.button("GradCAM"):
+        st.image(grid_image)
 
 
 
